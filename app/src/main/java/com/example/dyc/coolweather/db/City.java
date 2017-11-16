0a37a78e022253fdb9b50a3cbea5f703286e8595
dyc <m15221082470@163.com>
2017-11-16 20:01:20 +0800

    加入数据库和表

---
+++ app/src/main/java/com/example/dyc/coolweather/db/City.java
@@ -0,0 +1,37 @@
+package com.example.dyc.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by DYC on 2017/11/16.
+ */
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+}
